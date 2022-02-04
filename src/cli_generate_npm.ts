1c751d7527e7fda10a33ec477c0fe88ee25167e0
John Spurlock <john@skymethod.com>
2022-02-04 08:32:56 -0600

    Fix npm generator after src migration.

--- src/cli_generate_npm.ts
+++ src/cli_generate_npm.ts
@@ -11,19 +11,19 @@ export async function generateNpm(_args: (string | number)[], _options: Record<s
 
 async function generateEsmMainJs() {
     const contents = await generateBundleContents();
-    await saveContentsIfChanged('../npm/threadcap/esm/main.js', contents);
+    await saveContentsIfChanged('../../npm/threadcap/esm/main.js', contents);
 }
 
 async function generateCjsMainJs() {
     const contents = (await generateBundleContents())
         .replaceAll(/export { ([A-Z0-9a-z_]+) as ([A-Z0-9a-z_]+) };/g, 'exports.$2 = $1;');
 
-    await saveContentsIfChanged('../npm/threadcap/cjs/main.js', contents);
+    await saveContentsIfChanged('../../npm/threadcap/cjs/main.js', contents);
 }
 
 async function generateMainTypes() {
     // requires --unstable
-    const result = await (Deno as any).emit('./threadcap/threadcap.ts', { 
+    const result = await (Deno as any).emit(resolveRelativeFile('../threadcap/threadcap.ts'), { 
         // bundle: 'classic', 
         compilerOptions: { 
             declaration: true, // doesn't work with bundle: module
@@ -32,21 +32,25 @@ async function generateMainTypes() {
 
         },
     });
-    const contents = result.files['file:///Users/js/data/repos-llc/minipub/threadcap/threadcap.ts.d.ts']
-        .replaceAll(/\/\/\/ <amd-module name=".*?" \/>\s+/g, '');
-    await saveContentsIfChanged('../npm/threadcap/main.d.ts', contents);
+    const declaration = Object.entries(result.files).filter(v => v[0].endsWith('/threadcap.ts.d.ts'))[0][1] as string;
+    const contents = declaration.replaceAll(/\/\/\/ <amd-module name=".*?" \/>\s+/g, '');
+    await saveContentsIfChanged('../../npm/threadcap/main.d.ts', contents);
 }
 
 async function generateBundleContents(): Promise<string> {
     // requires --unstable
-    const result = await (Deno as any).emit('./threadcap/threadcap.ts', { 
+    const result = await (Deno as any).emit(resolveRelativeFile('../threadcap/threadcap.ts'), { 
         bundle: 'module',
     });
     return result.files['deno:///bundle.js'];
 }
 
+function resolveRelativeFile(relativePath: string): string {
+    return resolve(fromFileUrl(import.meta.url), relativePath);
+}
+
 async function saveContentsIfChanged(relativePath: string, contents: string) {
-    const outFile = resolve(fromFileUrl(import.meta.url), relativePath);
+    const outFile = resolveRelativeFile(relativePath);
     const filename = basename(outFile);
     const existing = await tryReadTextFile(outFile);
     if (existing !== contents) {
