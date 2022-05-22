95fb1c4a8d39fd03e5e298232388a4ab1ad64188
John Spurlock <john@skymethod.com>
2022-05-21 19:32:03 -0500

    Update deps to latest denoflare.

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectNamespace, DurableObjectState, DurableObjectStorage, DurableObjectStorageTransaction, DurableObjectStorageValue } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/common/cloudflare_workers_types.d.ts';
-export { ColoFromTrace } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/examples/tiered-websockets-worker/colo_from_trace.ts';
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/common/bytes.ts';
+export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectNamespace, DurableObjectState, DurableObjectStorage, DurableObjectStorageTransaction, DurableObjectStorageValue } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/cloudflare_workers_types.d.ts';
+export { ColoFromTrace } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/examples/tiered-websockets-worker/colo_from_trace.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/bytes.ts';
 export { chunk } from 'https://deno.land/std@0.131.0/collections/chunk.ts';
 export { toIMF } from 'https://deno.land/std@0.131.0/datetime/mod.ts';
 export { encode as encodeAscii85 } from 'https://deno.land/std@0.131.0/encoding/ascii85.ts';
