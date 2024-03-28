be6e41a525545ddce358bf05075411fd4929d990
John Spurlock <john@skymethod.com>
2024-03-27 20:02:37 -0500

    threadcap: Start fleshing out nostr comments and commenters.

--- src/threadcap/threadcap_nostr.ts
+++ src/threadcap/threadcap_nostr.ts
@@ -4,37 +4,112 @@ import { ProtocolImplementation, ProtocolMethodOptions, ProtocolUpdateMethodOpti
 export const NostrProtocolImplementation: ProtocolImplementation = {
 
     async initThreadcap(url: string, opts: ProtocolMethodOptions): Promise<Threadcap> {
-        const { debug } = opts;
+        const { debug, updateTime = new Date().toISOString() } = opts;
         const { protocol, hostname, searchParams } = destructureThreadcapUrl(url);
         const m = /^30311:([0-9a-f]{64}):(.*?)$/.exec(searchParams.get('space') ?? '');
         if (protocol !== 'nostr:' || !m) throw new Error(`Threadcap nostr urls should be in this form: nostr://<relay-server>?space=30311:<64-hexchars>:<identifer>`);
         const [ space, _, identifier ] = m;
+        const limit = parseInt(searchParams.get('limit') ?? '1000');
         const nodes: Record<string, Node> = {};
         const commenters: Record<string, Commenter> = {};
         
+        const makeUri = ({ message }: { message: string | undefined }) => {
+            const u = new URL(`${protocol}//${hostname}`);
+            u.searchParams.set('space', space);
+            if (message !== undefined) u.searchParams.set('message', message);
+            return u.toString();
+        }
+
         const state: Record<string, unknown> = {};
-        const activities = await query({ 
-            kinds: [ 30311 ],
-            limit: 100000,
-            tags: {
-                '#d': [ identifier ],
-            }
-        }, { hostname, debug, state });
+        const pubkeys = new Set<string>();
 
-        const activity = activities.filter(v => v.tags.some(v => v[0] === 'd' && v[1] === identifier));
-        console.log({ activity });
-
+        // query for the messages
         const messages = await query({ 
             kinds: [ 1311 ],
-            limit: 100000,
+            limit,
             tags: {
                 '#a': [ space ],
             }
         }, { hostname, debug, state });
 
-        const uri = url;
+        for (const message of messages) {
+            const uri = makeUri({ message: message.id });
+            nodes[uri] = {
+                comment: {
+                    attachments: [],
+                    attributedTo: message.pubkey,
+                    content: { un: message.content },
+                    published: new Date(message.created_at * 1000).toISOString(),
+                },
+                commentAsof: updateTime,
+                replies: [],
+                repliesAsof: updateTime,
+            };
+            pubkeys.add(message.pubkey);
+        }
 
-        return { protocol: 'nostr', roots: [ uri ], nodes, commenters };
+        // query for the activity
+        const activities = await query({ 
+            kinds: [ 30311 ],
+            tags: {
+                '#d': [ identifier ],
+            }
+        }, { hostname, debug, state });
+        const activity = activities.find(v => v.tags.some(v => v[0] === 'd' && v[1] === identifier));
+        const rootUri = makeUri({ message: undefined });
+        nodes[rootUri] = {
+            comment: {
+                attachments: [],
+                attributedTo: activity ? activity.pubkey : '',
+                content: { un: activity?.tags.find(v => v[0] === 'title')?.at(1) ?? '' },
+                published: activity ? new Date(activity.created_at * 1000).toISOString() : undefined,
+            },
+            commentAsof: updateTime,
+            replies: Object.keys(nodes),
+            repliesAsof: updateTime,
+        };
+        if (activity) pubkeys.add(activity.pubkey);
+
+        // resolve profiles for pubkeys
+        const relaysForProfiles = [ 'relay.primal.net', 'relay.damus.io', 'relay.snort.social' ];
+        const remainingPubkeys = new Set(pubkeys);
+        const allProfiles: NostrEvent[] = [];
+        const resolvedByHostname: Record<string, number> = {};
+        for (const relayForProfiles of relaysForProfiles) {
+            let resolved = 0;
+            const profiles = await query({ kinds: [ 0 ], authors: [ ...remainingPubkeys ] }, { hostname: relayForProfiles, state, debug });
+            allProfiles.push(...profiles);
+            for (const profile of profiles) {
+                remainingPubkeys.delete(profile.pubkey);
+                resolved++;
+            }
+            resolvedByHostname[relayForProfiles] = resolved;
+
+            if (remainingPubkeys.size === 0) break;
+        }
+        resolvedByHostname['unresolved'] = pubkeys.size - Object.values(resolvedByHostname).reduce((prev, cur) => prev + cur, 0);
+
+        for (const pubkey of pubkeys) {
+            const profile = allProfiles.find(v => v.pubkey === pubkey);
+            if (profile) {
+                const { name, display_name, picture, website, lud16 } = JSON.parse(profile.content) as NostrProfile;
+
+                commenters[pubkey] = {
+                    name: display_name ?? name ?? pubkey,
+                    fqUsername: lud16,
+                    icon: picture ? { url: picture } : undefined,
+                    url: website,
+                    asof: updateTime,
+                };
+            } else {
+                commenters[pubkey] = {
+                    name: pubkey,
+                    asof: updateTime,
+                };
+            }
+        }
+        if (debug) console.log(JSON.stringify({ resolvedByHostname }));
+        return { protocol: 'nostr', roots: [ rootUri ], nodes, commenters };
     },
     
     async fetchComment(id: string, opts: ProtocolUpdateMethodOptions): Promise<Comment> {
@@ -93,7 +168,15 @@ type NostrEvent = {
     sig: string,
 };
 
-async function query(filter: { kinds?: number[], limit?: number, tags?: Record<string, string[]> }, opts: { hostname: string, debug?: boolean, state: Record<string, unknown> }): Promise<NostrEvent[]> {
+type NostrProfile = {
+    name?: string,
+    display_name?: string,
+    website?: string,
+    picture?: string,
+    lud16?: string,
+};
+
+async function query(filter: { kinds?: number[], limit?: number, tags?: Record<string, string[]>, authors?: string[] }, opts: { hostname: string, debug?: boolean, state: Record<string, unknown> }): Promise<NostrEvent[]> {
     const { hostname, debug, state } = opts;
 
     const ws = await (async () => {
@@ -134,7 +217,7 @@ async function query(filter: { kinds?: number[], limit?: number, tags?: Record<s
             if (!Array.isArray(parsed)) throw new Error(`Unexpected payload`);
             const [ first, ...rest ] = parsed;
             if (first === 'EOSE') {
-                const [ sub ] = rest;
+                const [ _sub ] = rest;
                 // if (sub === subscriptionId) send([ 'CLOSE', subscriptionId ]); // TODO when server supports this
                 resolve(rt);
             } else if (first === 'CLOSED') {
@@ -155,7 +238,7 @@ async function query(filter: { kinds?: number[], limit?: number, tags?: Record<s
         reject(msg);
     });
     
-    const { kinds, limit, tags } = filter;
-    send([ 'REQ', subscriptionId, { kinds, limit, ...tags } ]);
+    const { kinds, limit, tags, authors } = filter;
+    send([ 'REQ', subscriptionId, { kinds, limit, ...tags, authors } ]);
     return promise;
 }
